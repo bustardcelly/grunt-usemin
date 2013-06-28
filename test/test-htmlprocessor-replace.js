862a01bae7107e7b1ac9c2cd28dfe9cfe135a4df
Todd Anderson <bustardcelly@gmail.com>
2013-06-28 05:58:11 -0400

    added detect for build:replace on requirejs defined blocks.

---
+++ test/test-htmlprocessor-replace.js
@@ -0,0 +1,34 @@
+'use strict';
+var assert = require('assert');
+var HTMLProcessor = require('../lib/htmlprocessor');
+
+describe('htmlprocessor', function () {
+
+  it('should detect and handle the usage on RequireJS in blocks', function () {
+    var htmlcontent = '<!-- build:replace scripts/amd-app.js -->\n' +
+    '<script data-main="scripts/main" src="scripts/vendor/require.js"></script>\n' +
+    '<!-- endbuild -->';
+    var hp = new HTMLProcessor('', '', htmlcontent, 3);
+    assert.equal(1, hp.blocks.length);
+    assert.ok(hp.blocks[0].requirejs);
+    assert.equal('replace', hp.blocks[0].type);
+    assert.equal('scripts/amd-app.js', hp.blocks[0].requirejs.dest);
+    assert.equal('scripts', hp.blocks[0].requirejs.baseUrl);
+    assert.equal('scripts/vendor/require.js', hp.blocks[0].requirejs.src);
+    assert.equal('main', hp.blocks[0].requirejs.name);
+  });
+
+  describe('replaceWith', function() {
+
+    it('should return a string that will replace the furnished block (RequireJS)', function () {
+      var htmlcontent = '<!-- build:replace scripts/amd-app.js -->\n' +
+      '<script data-main="scripts/main" src="scripts/vendor/require.js"></script>\n' +
+      '<!-- endbuild -->\n';
+      var hp = new HTMLProcessor('', '', htmlcontent, 3);
+      var replacestring = hp.replaceWith(hp.blocks[0]);
+      assert.equal('<script src="scripts/amd-app.js"></script>', replacestring);
+    });
+
+  });
+
+});
